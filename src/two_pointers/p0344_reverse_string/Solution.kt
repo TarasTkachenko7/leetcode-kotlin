be575d9d1b662f6c3cf1048569254b72b308b10f
Taras <fartuch.kek@gmail.com>
2026-09-17 17:34:50 +0300

    docs(two-pointers): добавил решение в файл 0344 Reverse String

--- src/two_pointers/p0344_reverse_string/Solution.kt
+++ src/two_pointers/p0344_reverse_string/Solution.kt
@@ -1,4 +1,17 @@
 package two_pointers.p0344_reverse_string
 
 class Solution {
+    fun reverseString(s: CharArray): Unit {
+        var left = 0
+        var right = s.lastIndex
+
+        while (left < right) {
+            val temp = s[left]
+            s[left] = s[right]
+            s[right] = temp
+
+            left++
+            right--
+        }
+    }
 }
